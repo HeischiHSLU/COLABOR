840c215ea673d61c36f57e54b2f9a8de7723b542
HeischiHSLU <hannes.salzmann@stud.hslu.ch>
2024-05-31 13:13:07 +0200

    explosion bis phase 4

--- Sprint4/Experimente/Explosion/sketch.js
+++ Sprint4/Experimente/Explosion/sketch.js
@@ -1,11 +1,12 @@
 let particles = [];
 let planets = [];
-let explosionAmmount = 150;
+let explosionAmmount = 180;
 let novaRotationSpeed = 0.1;
 
 let phase1 = true;
 let phase2 = false;
 let phase3 = false;
+let phase4 = false;
 
 
 
@@ -19,11 +20,17 @@ let breackPoint = 1000; // after 10 seconds
 function setup() {
   createCanvas(windowWidth,windowHeight,WEBGL);
   angleMode(DEGREES);
+
+  for(let i = 0; i < 5; i++){
+    planets.push(new Planet());
+  }
 }
+
+
 
 function draw() {
   
-  rotateX(PI/3.2);
+  rotateX(PI/4);
 
   rotateY(frameCount * 0.1);
   background(0,0,20);
@@ -32,9 +39,16 @@ function draw() {
 
   directionalLight( [255], createVector(0,0,-1) );
 
-  nova();
 
   if(phase1){
+    nova();
+
+    for(let i = 0; i < 5; i++){
+      console.log("show planet");
+      planets[i].show();
+
+    }
+
     if(frameCount < breackPointPhase1){
       
       phase1 = true;
@@ -43,47 +57,81 @@ function draw() {
     }
     else{
       phase1 = false;
+      phase2 = true;
       lastCheckpoint = frameCount;
     }
   }
+  if(phase2){
+
+    
+    nova();
+
+    speedUp();
+
+    
+    
+  }
+  if(phase3){
+
+    for( let i = 0; i < particles.length; i++){
+
+      if(particles[i].bounces > 1000){ // its random number, just to make sure it will stop at some point
+        // phase3 = false;
+        // phase4 = true;
+         console.log("phase4")
+      }
+
+      if(dist(particles[i].pos.x,particles[i].pos.y,particles[i].pos.z,0,0,0) > 5000){
+        particles.splice(i,1);
+      }
+      particles[i].update();
+      particles[i].show();
+      
+      if(dist(particles[i].pos.x,particles[i].pos.y,particles[i].pos.z,0,0,0) > 2000){
+        particles[i].vel.mult(-1);
+        particles[i].reversed = true;
+      
+      }
+      if(particles[i].reversed == true && dist(particles[i].pos.x,particles[i].pos.y,particles[i].pos.z,0,0,0) < 200){
+        particles[i].vel.mult(-0.5);
+        particles[i].stagination = true;
+        if(i == 1){console.log("Bounce!")}
+        
+        particles[i].bounces++;
+      }
+      if(particles[i].stagination == true && dist(particles[i].pos.x,particles[i].pos.y,particles[i].pos.z,0,0,0) > 250){
+        particles[i].vel.mult(-1);
+        particles[i].bounces++;
+        if(i == 1){console.log("Bounce!")}
+      }
+    }
+
+
+    for(let i = 0; i < 5; i++){
+      planets[i].update();
+      planets[i].show();
+    }
+  }
+  if (phase4){
+
+  }
 
   
 
-  speedUp();
-
-  for( let i = 0; i < particles.length; i++){
-    if(dist(particles[i].pos.x,particles[i].pos.y,particles[i].pos.z,0,0,0) > 2500){
-      particles.splice(i,1);
-    }
-    particles[i].update();
-    particles[i].show();
-    if(dist(particles[i].pos.x,particles[i].pos.y,particles[i].pos.z,0,0,0) > 2000){
-      particles[i].vel.mult(-1);
-      particles[i].reversed = true;
-    
-    }
-    if(particles[i].reversed == true && dist(particles[i].pos.x,particles[i].pos.y,particles[i].pos.z,0,0,0) < 200){
-      particles[i].vel.mult(-0.5);
-      particles[i].stagination = true;
-    }
-    if(particles[i].stagination == true && dist(particles[i].pos.x,particles[i].pos.y,particles[i].pos.z,0,0,0) > 250){
-      particles[i].vel.mult(-1);
-    }
-  }
+  
 }
-
 function nova(){
   noStroke();
   
-
   push();
   rotateX(frameCount * novaRotationSpeed);
   rotateY(frameCount * novaRotationSpeed * 0.5);
-  //sphere(novaSize);
-  torus(novaSize, novaSize/5);
+  sphere(novaSize);
+  torus(novaSize-10, novaSize/5);
 
   pop();
 }
+
 function speedUp(){
 
   let normalizedFrameCount = frameCount - lastCheckpoint;
@@ -100,14 +148,21 @@ function speedUp(){
 
     }
 
+    for(let i = 0; i < 5; i++){
+      planets[i].show();
+
+    }
   }else{
     if (breackPoint < normalizedFrameCount && normalizedFrameCount < breackPoint + 10){
       if(random(1) > 0.5){
         for(let i = 0; i < explosionAmmount; i++){
           particles.push(new Particles());
+          phase2 = false;
+          phase3 = true;
         }
       }
     }
+    
     novaRotationSpeed = 0;
     return;
   }
@@ -118,10 +173,13 @@ function speedUp(){
 class Particles{
   constructor() {
     this.pos = createVector(0,0,0);
-    this.vel = p5.Vector.random3D().normalize().mult(random(4,6));
+    this.vel = p5.Vector.random3D().normalize().mult(random(4,5));
     this.size = random(6,10);
     this.reversed = false;
     this.stagination = false;
+    this.bounces = 0;
+    this.destination = createVector(0,0,0);
+    this.tempPos = createVector(0,0,0);
   }
   update(){
     this.pos.add(this.vel)
@@ -136,23 +194,63 @@ class Particles{
     sphere(this.size);
     pop();
   }
+  changeDirection(){
+    this.vel.mult(-1);
+    this.reversed = !this.reversed;
+  }
+  peak(intensity){
+    this.tempPos = this.pos.copy();
+    this.destination = this.pos.copy().normalize().add(intensity);
+    this.vel = this.destination.sub(this.tempPos).mult(0.5);
+    this.reversed = false;
+
+  }
 }
 
 class Planet{
   constructor(){
-    this.pos = createVector(0,0,0);
-    this.size = 100;
+    let randomExclude = 0;
+    if(random(0,100) > 50){
+      randomExclude = (random(-400,-200));
+    }else{
+      randomExclude = (random(200,400));
+    }
+    this.pos = createVector(randomExclude,0,random(-800,400));
+    this.size = random(25,75) * 0.7;
+    this.vel = this.pos.copy().normalize().mult(4);
   }
+  update(){
+    this.pos.add(this.vel);
+  }
+  
   show(){
     push();
     noStroke();
     fill(255);
     translate(this.pos.x,this.pos.y,this.pos.z);
+    rotateY(frameCount * -0.5);
+    rotateX( this.size * this.size  *-2 );
+    torus(this.size, this.size/5);
     sphere(this.size);
     pop();
   }
 
 }
+
+function phase3Peak(intensity){
+  for(let i = 0; i < explosionAmmount; i++){
+    particles[i].peak(intensity);
+    if(dist(particles[i].pos.x,particles[i].pos.y,particles[i].pos.z, this.tempPos.x,this.tempPos.y,this.tempPos.z) < intensity){
+      if(particles[i].reversed == true){
+        particles[i].pos = this.destination.copy();
+        particles[i].vel = 0;
+      }
+      particles[i].pos = this.destination.copy();
+      particles[i].changeDirection();
+    }
+  }
+}
+
 
 function easeInQuad(t){
   return t * t;
